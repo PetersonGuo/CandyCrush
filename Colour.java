8628e854afa288cc5ee26fb35099b44ed87329fc
Peterson Guo <petersonguo@gmail.com>
2023-06-13 10:57:50 -0400

    Add enum

---
+++ Colour.java
@@ -0,0 +1,28 @@
+/**
+ * Write a description of class CANDY_COLOUR here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public enum Colour  {
+    RED("Red"),
+    ORANGE("Orange"),
+    YELLOW("Yellow"),
+    GREEN("Green"),
+    BLUE("Blue"),
+    PURPLE("Purple");
+    private static final Colour[] values = values();
+    private String colour;
+    private Colour(String colour) {
+        this.colour = colour;
+    }
+    
+    public static Colour random() {
+        return values[(int)(Math.random() * values.length)];
+    }
+    
+    @Override
+    public String toString(){
+        return colour;
+    }
+}
